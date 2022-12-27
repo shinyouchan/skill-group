0b7892c14967b9ee65c0c5abd5dbc4d7bf5de3d2
shinyouchan <youchan9895@gmail.com>
2022-12-27 19:48:46 +0900

    12/27webpack 수업

---
+++ react/dist/bundle.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*************************!*\
+  !*** ./src/js/index.js ***!
+  \*************************/
+console.log('hi!')
+function sum(a,b) {
+    console.log(a + b) 
+}
+sum(1,2)
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ react/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path")
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+module.exports = {
+    entry: "./react/src/js/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "./dist") ,
+        clean: true
+    },
+    devtool: "source-map",
+    mode: "development",
+    plugins:[
+        new HtmlWebpackPlugin({
+            title: "keyboard",
+            template: "./index.html",
+            inject: "body",
+            favicon: "./favicon.ico"
+        }),
+        new MiniCssExtractPlugin({filename: "style.css"})
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            }
+        ]
+    },
+    optimization: {
+        minimizer: [
+            new TerserWebpackPlugin(),
+            new CssMinimizerPlugin()
+        ]
+    }
+}
