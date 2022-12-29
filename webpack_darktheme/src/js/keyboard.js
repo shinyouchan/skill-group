362784f5e42e95dc359f7f25f6a16d633c2aa738
shinyouchan <youchan9895@gmail.com>
2022-12-29 19:16:56 +0900

    update program

--- webpack_darktheme/src/js/keyboard.js
+++ webpack_darktheme/src/js/keyboard.js
@@ -1,13 +1,21 @@
 export class Keyboard {
     #switchEl
     #fontSelectEl
+    #containerEl
+    #keyboardEl
+    #inputGroupEl
+    #inputEl
     constructor() {
         this.#assignElement()
         this.#addEvent()
     }
     #assignElement() {
-        this.#switchEl = document.getElementById('switch')
-        this.#fontSelectEl = document.getElementById('font')
+        this.#containerEl = document.getElementById('container')
+        this.#switchEl = this.#containerEl.querySelector('#switch')
+        this.#fontSelectEl = this.#containerEl.querySelector('#font')
+        this.#keyboardEl = this.#containerEl.querySelector('#keyboard')
+        this.#inputGroupEl = this.#containerEl.querySelector('#input-group')
+        this.#inputEl = this.#containerEl.querySelector('#input')
     }
     #addEvent() {
         this.#switchEl.addEventListener('change', (e) => {
@@ -18,6 +26,20 @@ export class Keyboard {
         })
         this.#fontSelectEl.addEventListener('change', (e) => {
             document.body.style.fontFamily = e.target.value
+        })
+        document.addEventListener('keydown', (e) => {
+            this.#keyboardEl.querySelector(`[data-code=${e.code}]`)?.classList.add('active')
+            this.#inputGroupEl.classList.toggle (
+                'error', 
+                e.key==='Process'
+            )
+
+        })
+        document.addEventListener('keyup', (e) => {
+            this.#keyboardEl.querySelector(`[data-code=${e.code}]`)?.classList.remove('active')
+        })
+        this.#inputEl.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[ㄱ-ㅎ| ㅏ-ㅣ | 가-힣]/ , '')
         })
     }
 }
